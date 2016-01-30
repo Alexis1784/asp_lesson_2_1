30ccca2be56a1ff09bea10facc471174fd008cd3
Alex <alexis1784@gmail.com>
2016-01-30 17:13:01 +0500

    working version

---
+++ asp_lesson_2_1/Models/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace asp_lesson_2_1.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public BookContext() : base("asp_lesson_2_1_in_base_b") { }
+    }
+}
